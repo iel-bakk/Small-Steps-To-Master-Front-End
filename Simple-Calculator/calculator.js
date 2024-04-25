b7a47049b5f00a9e1e57135a54b792c95e2b37b5
iel-bakk <jesus1337@gmail.com>
2024-04-25 15:52:03 +0100

    updated a lotttt of stuff

--- Simple-Calculator/calculator.js
+++ Simple-Calculator/calculator.js
@@ -3,13 +3,21 @@ const main = document.createElement("main")
 // creating the three sections of the calculator, check the readMe file to understand
 
 const firstSection = document.createElement("div")
+    firstSection.className = "firstSection"
 const secondSection = document.createElement("div")
+    secondSection.className = "secondSection"
 const thirdSection = document.createElement("div")
+    thirdSection.className = "thirdSection"
 
 // create first section elements
 const firstSectionInput = document.createElement("input")
 const firstSectionDelete = document.createElement("button")
     firstSectionDelete.textContent = "DEL"
+    firstSectionDelete.addEventListener("click", ()=> {
+        const tmp = firstSectionInput.value.split('')
+        tmp.pop()
+        firstSectionInput.value = tmp.join('')
+    })
 
     // add elements to the first section .
         firstSection.appendChild(firstSectionInput)
@@ -19,20 +27,32 @@ const firstSectionDelete = document.createElement("button")
 
     // first Sub Section
     const firstSubSection = document.createElement("section")
+    firstSubSection.className = "numbers"
         for (let i = 0; i < 10; i++) {
             const button = document.createElement("button")
             button.textContent = i
             // add the event listener to change the calculation formula
+            button.addEventListener("click", (e)=> {
+                firstSectionInput.value += e.target.textContent;
+                console.log("clicked on a number : ", e.target.textContent)
+                console.log("input text content is ", firstSectionInput.textContent)
+            })
             firstSubSection.appendChild(button)
         }
 
     // Second Sub Section
     const secondSubSection = document.createElement("section")
-
+        secondSubSection.className = "signs"
     const plusButton = document.createElement("button")
         plusButton.textContent = "+"
+        plusButton.addEventListener("click", ()=> {
+            firstSectionInput.value += " + "
+        })
     const minusButton = document.createElement("button")
         minusButton.textContent = "-"
+        minusButton.addEventListener("click", ()=> {
+            firstSectionInput.value += " - "
+        })
 
     secondSubSection.appendChild(plusButton)
     secondSubSection.appendChild(minusButton)
@@ -47,6 +67,9 @@ secondSection.appendChild(secondSubSection)
         equalButton.textContent = "="
     const ClearButton = document.createElement("button")
         ClearButton.textContent = "CLEAR"
+        ClearButton.addEventListener("click", ()=> {
+            firstSectionInput.value = ""
+        })
 
 // add elements to the third section
 thirdSection.appendChild(ClearButton)
